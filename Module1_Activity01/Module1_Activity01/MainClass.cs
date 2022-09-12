78c9592ace540c7aef49869cdfb4eea6c40a31c0
nakpi <nakpi@DESKTOP-C0VPPF3>
2022-09-12 20:03:33 +0800

    Add project files.

---
+++ Module1_Activity01/Module1_Activity01/MainClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Module1_Activity01
+{
+    public class MainClass
+    {
+        public static double NormalFontSize = 24;
+
+        public static readonly Color BColor = Color.MediumPurple;
+    }
+}
